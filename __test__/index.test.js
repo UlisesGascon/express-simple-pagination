08a4776f40cffed676566b9a814a5564227f595c
Ulises Gascon <ulises.gascon@guidesmiths.com>
2020-03-16 11:31:49 +0100

    Improved coverage

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -14,6 +14,7 @@ describe('Pagination middleware', () => {
   test('No pagination requested', () => {
     pagination()(request, response, next)
     expect(request.pagination.isEnable).toBe(false)
+    expect(request.pagination.default).toStrictEqual({ limit: 20, offset: 0 })
   })
 
   test('Default configuration', () => {
@@ -22,12 +23,14 @@ describe('Pagination middleware', () => {
     pagination()(request, response, next)
     expect(request.pagination.isEnable).toBe(true)
     expect(request.pagination.current).toStrictEqual({ limit: 20, offset: 0 })
+    expect(request.pagination.default).toStrictEqual({ limit: 20, offset: 0 })
 
     // Max by default: 500
     request.query.limit = '10000'
     pagination()(request, response, next)
     expect(request.pagination.isEnable).toBe(true)
     expect(request.pagination.current).toStrictEqual({ limit: 500, offset: 0 })
+    expect(request.pagination.default).toStrictEqual({ limit: 20, offset: 0 })
   })
 
   test('Custom Configuration', () => {
@@ -36,12 +39,14 @@ describe('Pagination middleware', () => {
     pagination({ min: 10 })(request, response, next)
     expect(request.pagination.isEnable).toBe(true)
     expect(request.pagination.current).toStrictEqual({ limit: 10, offset: 0 })
+    expect(request.pagination.default).toStrictEqual({ limit: 10, offset: 0 })
 
     // Max by default: 500
     request.query.limit = '1000'
     pagination({ max: 1000 })(request, response, next)
     expect(request.pagination.isEnable).toBe(true)
     expect(request.pagination.current).toStrictEqual({ limit: 1000, offset: 0 })
+    expect(request.pagination.default).toStrictEqual({ limit: 20, offset: 0 })
   })
 
   test('offset behaviour', () => {
@@ -50,12 +55,14 @@ describe('Pagination middleware', () => {
     pagination()(request, response, next)
     expect(request.pagination.isEnable).toBe(true)
     expect(request.pagination.current).toStrictEqual({ limit: 20, offset: 0 })
+    expect(request.pagination.default).toStrictEqual({ limit: 20, offset: 0 })
 
     // Valid transformation
     request.query.offset = '500'
     pagination()(request, response, next)
     expect(request.pagination.isEnable).toBe(true)
     expect(request.pagination.current).toStrictEqual({ limit: 20, offset: 500 })
+    expect(request.pagination.default).toStrictEqual({ limit: 20, offset: 0 })
   })
 
   test('limit behaviour', () => {
@@ -64,23 +71,27 @@ describe('Pagination middleware', () => {
     pagination()(request, response, next)
     expect(request.pagination.isEnable).toBe(true)
     expect(request.pagination.current).toStrictEqual({ limit: 20, offset: 0 })
+    expect(request.pagination.default).toStrictEqual({ limit: 20, offset: 0 })
 
     // low value
     request.query.limit = '10'
     pagination()(request, response, next)
     expect(request.pagination.isEnable).toBe(true)
     expect(request.pagination.current).toStrictEqual({ limit: 20, offset: 0 })
+    expect(request.pagination.default).toStrictEqual({ limit: 20, offset: 0 })
 
     // high value
     request.query.limit = '1500'
     pagination()(request, response, next)
     expect(request.pagination.isEnable).toBe(true)
     expect(request.pagination.current).toStrictEqual({ limit: 500, offset: 0 })
+    expect(request.pagination.default).toStrictEqual({ limit: 20, offset: 0 })
 
     // Regular value
     request.query.limit = '456'
     pagination()(request, response, next)
     expect(request.pagination.isEnable).toBe(true)
     expect(request.pagination.current).toStrictEqual({ limit: 456, offset: 0 })
+    expect(request.pagination.default).toStrictEqual({ limit: 20, offset: 0 })
   })
 })
